b60ed9deff48c19e7f5875de8af1bd931280d51b
v-palaks <v-palaks@MADHU-SURFACE>
2016-04-22 23:10:53 -0700

    Add project files.

---
+++ ElementsofProgramming/13.7.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElementsofProgramming
+{
+    class _13
+    {
+
+        public void ShortestDistance()
+        {
+            //string[] str = new string[] { "sripts", "signed", "run", "only", "to", "policy", "the", "force", "can", "you", "metadata", "UNC", "the", "contain", "don't", "least", "at", "or", "Internet", "the", "from", "downloaded", "been", "not", "have", "that", "scripts", "own", "your", "on", "restrictions", "internal", "the", "up", "tighten", "to", "want", "you", "If" };
+
+            string[] str = new string[] { "results","result","no","and","fun","no","work","no","for","makes","play","no","and","Work","All"        };
+
+            Hashtable ht = new Hashtable();
+            int shortest = int.MaxValue;
+            for(int i=0;i<str.Length;i++)
+            {
+                if(ht.ContainsKey(str[i]))
+                {
+
+                    int temp = Convert.ToInt32(ht[str[i]]);
+                    int distance = i - temp;
+                    if(distance <shortest)
+                    {
+                        shortest = distance;
+                    }
+                    ht[str[i]] = i;
+
+                }
+                else
+                {
+                    ht.Add(str[i], i);
+
+                }
+
+            }
+
+
+
+        }
+
+        
+
+        
+
+    }
+}
---
+++ ElementsofProgramming/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ElementsofProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            _13 func = new _13();
+
+            func.ShortestDistance();
+        }
+    }
+}
